24957dfdc48e9c2553bbac7488846e9795815089
chenghaopeng <i@chper.cn>
2019-05-27 09:51:55 +0800

    ADD error status; COMBINE game.py TO client.py

--- client.py
+++ client.py
@@ -4,8 +4,81 @@ import webbrowser
 import sys
 from http.server import HTTPServer,BaseHTTPRequestHandler
 import io,shutil,urllib
+import json
+import subprocess
+import os
+import websocket
 
-import game
+cfg = {}
+
+def is_json(str):
+    try:
+        json.loads(str)
+    except ValueError:
+        return False
+    return True
+
+def ws_send(ws, op, data):
+    global cfg
+    ws.send(json.dumps({'op': op, 'data': data}))
+
+def on_message(ws, message):
+    print("Got new message from server.")
+    global cfg
+    res = json.loads(message)
+    if res['status'] == "win" or res['status'] == "lose" or res['status'] == "draw":
+        print("The result is " + res['status'] + ".")
+        sys.exit(0)
+    elif res['status'] == "error":
+        print("Your token is invalid.")
+        sys.exit(0)
+    elif res['status'] == "wrong":
+        print("Your AI application output a invaild result!")
+        print("You may have to edit your AI application.")
+        os.system("pause")
+    print("It's your turn.")
+    while True:
+        print("Running your AI application.")
+        sp = subprocess.Popen([cfg['path'], res['data']], stdout=subprocess.PIPE)
+        dat = sp.stdout.readline()
+        if is_json(dat):
+            break
+        print("Your AI application output a wrong JSON string. Please check your application.")
+        os.system("pause")
+    print("Send your solution to server.")
+    ws_send(ws, 'operation', dat)
+    print("Waiting for others' turn.")
+
+def on_error(ws, error):
+    print("Something Wrong Happened: ", end='.')
+    print(error)
+    print("Please check your configuraion or network.")
+    sys.exit(0)
+
+def on_close(ws):
+    print("Connection Closed.")
+    sys.exit(0)
+
+def on_open(ws):
+    print("Connect successful.")
+    print("Send prepare message to server.")
+    ws_send(ws, "prepare", {})
+    print("Waiting for server response.")
+
+def main():
+
+    global cfg
+    URL = cfg['url']
+    PTH = cfg['path']
+
+    if not os.path.exists(PTH):
+        print("AI application not exists.")
+        sys.exit(0)
+
+    print("Connecting to server " + URL + ".")
+    ws = websocket.WebSocketApp(URL, on_message = on_message, on_error = on_error, on_close = on_close)
+    ws.on_open = on_open
+    ws.run_forever()
 
 class MyHttpHandler(BaseHTTPRequestHandler):
     def log_message(self, format, *args):
@@ -28,6 +101,7 @@ class MyHttpHandler(BaseHTTPRequestHandler):
     
     def do_POST(self):
         global httpd
+        global cfg
         html = '<p class="title">Success!</p><p class="title">Please check in command line window,</p><p class="title">and watch the game in the webbrowser.</p>'.encode("UTF-8")
         self.send_response(200)
         self.send_header('Content-type','text/html; charset=utf-8')
@@ -35,8 +109,8 @@ class MyHttpHandler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(html)
         data = urllib.parse.parse_qs(urllib.parse.unquote(str(self.rfile.readline(), 'UTF-8')))
-        game.cfg = {'url': data['url'][0], 'path': data['path'][0].replace("file://", "")}
-        game.main()
+        cfg = {'url': data['url'][0], 'path': data['path'][0].replace("file://", "")}
+        main()
         sys.exit(0)
         httpd.shutdown()
         httpd.server_close()
